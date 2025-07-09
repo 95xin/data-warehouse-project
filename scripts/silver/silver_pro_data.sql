69235b88ba4bca4a4d156a5a486a0e5c494c9afc
xin <96448222+95xin@users.noreply.github.com>
2025-07-09 18:29:54 +0100

    Update silver_pro_data.sql

--- scripts/silver/silver_pro_data.sql
+++ scripts/silver/silver_pro_data.sql
@@ -41,80 +41,80 @@ SELECT
 ALTER TABLE silver.crm_prd_info ADD COLUMN cat_id VARCHAR(50);
 
 INSERT INTO silver.crm_prd_info(
-	   prd_id,
-	   cat_id,
-	   prd_key,
-	   prd_nm,
-	   prd_cost,
-	   prd_line,
-	   prd_start_dt,
-	   prd_end_dt
+			   prd_id,
+			   cat_id,
+			   prd_key,
+			   prd_nm,
+			   prd_cost,
+			   prd_line,
+			   prd_start_dt,
+			   prd_end_dt
 )
 
-SELECT prd_id,
-	   REPLACE (SUBSTRING(prd_key,1,5),'-','_') as cat_id,
-	   SUBSTRING (prd_key,7,LENGTH(prd_key)) as prd_key,
-	   prd_nm,
-	   COALESCE(prd_cost,0) as prd_cost,
-	   CASE WHEN UPPER(TRIM(prd_line))='M' THEN 'Mountain'
-	   		WHEN UPPER(TRIM(prd_line))='R' THEN 'Road'
-			WHEN UPPER(TRIM(prd_line))='S' THEN 'Other Sales'
-			WHEN UPPER(TRIM(prd_line))='T' THEN 'Tourin'
-			ELSE 'n/a'
-	   END AS prd_line,
-	   CAST(prd_start_dt AS DATE) as prd_start_dt,
-	   LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 as prd_end_dt
-FROM   bronze.crm_prd_info
+SELECT 		           prd_id,
+			   REPLACE (SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+			   SUBSTRING (prd_key,7,LENGTH(prd_key)) as prd_key,
+			   prd_nm,
+			   COALESCE(prd_cost,0) as prd_cost,
+			   CASE WHEN UPPER(TRIM(prd_line))='M' THEN 'Mountain'
+			   		WHEN UPPER(TRIM(prd_line))='R' THEN 'Road'
+					WHEN UPPER(TRIM(prd_line))='S' THEN 'Other Sales'
+					WHEN UPPER(TRIM(prd_line))='T' THEN 'Tourin'
+					ELSE 'n/a'
+			   END AS prd_line,
+			   CAST(prd_start_dt AS DATE) as prd_start_dt,
+			   LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 as prd_end_dt
+FROM   		           bronze.crm_prd_info
 
 
 -- lodaing data into silver.crm_sales_details
 
 INSERT INTO silver.crm_sales_details (
-    sls_ord_num,
-    sls_prd_key,
-    sls_cust_id,
-    sls_order_dt,
-    sls_ship_dt,
-    sls_due_dt,
-    sls_sales,
-    sls_quantity,
-    sls_price
+			    sls_ord_num,
+			    sls_prd_key,
+			    sls_cust_id,
+			    sls_order_dt,
+			    sls_ship_dt,
+			    sls_due_dt,
+			    sls_sales,
+			    sls_quantity,
+			    sls_price
 )
 SELECT 
-    sls_ord_num,
-    sls_prd_key,
-    sls_cust_id,
-    CASE 
-        WHEN sls_order_dt = 0 OR LENGTH(CAST(sls_order_dt AS TEXT)) != 8 THEN NULL
-        ELSE TO_DATE(CAST(sls_order_dt AS TEXT), 'YYYYMMDD')  -- Convert integer to date
-    END AS sls_order_dt,
-    CASE 
-        WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS TEXT)) != 8 THEN NULL
-        ELSE TO_DATE(CAST(sls_ship_dt AS TEXT), 'YYYYMMDD')  -- Convert integer to date
-    END AS sls_ship_dt,
-    CASE 
-        WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS TEXT)) != 8 THEN NULL
-        ELSE TO_DATE(CAST(sls_due_dt AS TEXT), 'YYYYMMDD')  -- Convert integer to date
-    END AS sls_due_dt,
-    CASE 
-        WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
-            THEN sls_quantity * ABS(sls_price)
-        ELSE sls_sales
-    END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
-    sls_quantity,
-    CASE 
-        WHEN sls_price IS NULL OR sls_price <= 0 
-            THEN sls_sales / COALESCE(sls_quantity, 0)
-        ELSE sls_price  -- Derive price if original value is invalid
-    END AS sls_price
-FROM bronze.crm_sales_details;
+			    sls_ord_num,
+			    sls_prd_key,
+			    sls_cust_id,
+			    CASE 
+			        WHEN sls_order_dt = 0 OR LENGTH(CAST(sls_order_dt AS TEXT)) != 8 THEN NULL
+			        ELSE TO_DATE(CAST(sls_order_dt AS TEXT), 'YYYYMMDD')  -- Convert integer to date
+			    END AS sls_order_dt,
+			    CASE 
+			        WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS TEXT)) != 8 THEN NULL
+			        ELSE TO_DATE(CAST(sls_ship_dt AS TEXT), 'YYYYMMDD')  -- Convert integer to date
+			    END AS sls_ship_dt,
+			    CASE 
+			        WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS TEXT)) != 8 THEN NULL
+			        ELSE TO_DATE(CAST(sls_due_dt AS TEXT), 'YYYYMMDD')  -- Convert integer to date
+			    END AS sls_due_dt,
+			    CASE 
+			        WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+			            THEN sls_quantity * ABS(sls_price)
+			        ELSE sls_sales
+			    END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+			    sls_quantity,
+			    CASE 
+			        WHEN sls_price IS NULL OR sls_price <= 0 
+			            THEN sls_sales / COALESCE(sls_quantity, 0)
+			        ELSE sls_price  -- Derive price if original value is invalid
+			    END AS sls_price
+FROM 			    bronze.crm_sales_details;
 
 
 -- loading data into silver.erp_cust_az12
 INSERT INTO silver.erp_cust_az12 (
-			cid,
-			bdate,
-			gen
+				cid,
+				bdate,
+				gen
 		)
 		SELECT
 			CASE
